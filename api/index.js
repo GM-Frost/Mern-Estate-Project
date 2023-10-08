64b1c24d97ff0667b4328f348a8fa8d15307e30e
Frosty <nayanbastola777@gmail.com>
2023-10-08 18:16:50 -0400

    connectToDatabase

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_SERVER_PORT)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB");
+  });
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Listening on 3000");
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Listening on ${process.env.SERVER_PORT}`);
 });
