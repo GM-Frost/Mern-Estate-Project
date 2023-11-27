d1a672c8459f72819b5807fa33023b31d855fa0b
Nayan <110303752+GM-Frost@users.noreply.github.com>
2023-11-27 00:34:10 -0500

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -32,7 +32,8 @@ const __dirname = path.resolve();
 const app = express();
 
 // Allow requests from frontend domain
-const whitelist = ["https://nova-estate.nayanbastola.com", "http://localhost:5173"];
+
+const whitelist = ["https://nova-estate.nayanbastola.com","http://nova-estate.nayanbastola.com", "http://localhost:5173"];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.includes(origin)) {
